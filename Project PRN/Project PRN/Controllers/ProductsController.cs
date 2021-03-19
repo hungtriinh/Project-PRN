2894761c1236a48dabfd5b8af38384e0160e0c8b
fullsirkool <64348207+fullsirkool@users.noreply.github.com>
2021-03-20 00:33:00 +0700

    Update 20/3/2021
    00:32AM Upload Product

--- Project PRN/Project PRN/Controllers/ProductsController.cs
+++ Project PRN/Project PRN/Controllers/ProductsController.cs
@@ -2,36 +2,32 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using PagedList;
 using Project_PRN.Models;
+using System.Configuration;
 
-namespace Project_PRN.Controllers
-{
-    public class ProductsController : Controller
-    {
+namespace Project_PRN.Controllers {
+    public class ProductsController : Controller {
         private ProjectPRNEntities3 db = new ProjectPRNEntities3();
 
         // GET: Products
-        public ActionResult Product()
-        {
+        public ActionResult Product() {
             return View();
         }
 
-        public JsonResult ProductJson(int? index, string searchText, int? userID)
-        {
-            if (index == null)
-            {
+        public JsonResult ProductJson(int? index, string searchText, int? userID) {
+            if (index == null) {
                 index = 1;
             }
             int pageSize = 9;
             int pageNumber = (index ?? 1);
             db.Configuration.ProxyCreationEnabled = false;
-            var listProduct = db.Products.ToList().Select(product => new Product
-            {
+            var listProduct = db.Products.ToList().Select(product => new Product {
                 productID = product.productID,
                 title = product.title,
                 author = product.author,
@@ -54,21 +50,18 @@ namespace Project_PRN.Controllers
 
         }
 
-        public ActionResult UploadProduct()
-        {
+        public ActionResult UploadProduct() {
             return View();
         }
 
-        public ActionResult ProductDetail(int? productID)
-        {
+        public ActionResult ProductDetail(int? productID) {
             ViewData["productID"] = productID;
             return View();
         }
 
         public JsonResult ProductDetailJson(int? productID) {
             db.Configuration.ProxyCreationEnabled = false;
-            Product products = db.Products.ToList().Select(product => new Product
-            {
+            Product products = db.Products.ToList().Select(product => new Product {
                 productID = product.productID,
                 title = product.title,
                 author = product.author,
@@ -102,31 +95,26 @@ namespace Project_PRN.Controllers
             return Json(products, JsonRequestBehavior.AllowGet);
         }
 
-        public JsonResult GetCategoriesJson()
-        {
+        public JsonResult GetCategoriesJson() {
             db.Configuration.ProxyCreationEnabled = false;
             List<Category> listCategories = db.Categories.ToList();
             return Json(listCategories, JsonRequestBehavior.AllowGet);
         }
 
         // GET: Products/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
+        public ActionResult Details(int? id) {
+            if (id == null) {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Product product = db.Products.Find(id);
-            if (product == null)
-            {
+            if (product == null) {
                 return HttpNotFound();
             }
             return View(product);
         }
 
         // GET: Products/Create
-        public ActionResult Create()
-        {
+        public ActionResult Create() {
             ViewBag.userID = new SelectList(db.Accounts, "userID", "email");
             ViewBag.categoriesID = new SelectList(db.Categories, "categoriesID", "categoriesName");
             return View();
@@ -137,30 +125,41 @@ namespace Project_PRN.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "productID,title,author,description,shortDescription,image,price,quantity,sold,postTime,categoriesID,userID")] Product product)
-        {
-            if (ModelState.IsValid)
-            {
+        public ActionResult Create([Bind(Include = "productID,title,author,description,shortDescription,image,price,quantity,sold,postTime,categoriesID,userID")] Product product, HttpPostedFileBase image) {
+            if (ModelState.IsValid) {
+                product.userID = Int32.Parse(Session["user"].ToString());
+                product.postTime = DateTime.Now;
+                string imgPath = ConfigurationManager.ConnectionStrings["imagePath"].ToString();
+                try {
+                    if (image != null) {
+                        string path = Path.Combine(Server.MapPath($"~{imgPath}"), Path.GetFileName(image.FileName));
+                        image.SaveAs(path);
+                        product.image = image.FileName;
+                    }
+                    ViewBag.FileStatus = "File uploaded successfully.";
+                } catch (Exception) {
+
+                    ViewBag.FileStatus = "Error while file uploading.";
+                }
+
                 db.Products.Add(product);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToRoute(new {
+                    controller = "Home",
+                    action = "Index",
+                    id = UrlParameter.Optional
+                });
             }
-
-            ViewBag.userID = new SelectList(db.Accounts, "userID", "email", product.userID);
-            ViewBag.categoriesID = new SelectList(db.Categories, "categoriesID", "categoriesName", product.categoriesID);
-            return View(product);
+            return RedirectToAction("UploadProduct");
         }
 
         // GET: Products/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
+        public ActionResult Edit(int? id) {
+            if (id == null) {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Product product = db.Products.Find(id);
-            if (product == null)
-            {
+            if (product == null) {
                 return HttpNotFound();
             }
             ViewBag.userID = new SelectList(db.Accounts, "userID", "email", product.userID);
@@ -173,10 +172,8 @@ namespace Project_PRN.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "productID,title,author,description,shortDescription,image,price,quantity,sold,postTime,categoriesID,userID")] Product product)
-        {
-            if (ModelState.IsValid)
-            {
+        public ActionResult Edit([Bind(Include = "productID,title,author,description,shortDescription,image,price,quantity,sold,postTime,categoriesID,userID")] Product product) {
+            if (ModelState.IsValid) {
                 db.Entry(product).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -187,15 +184,12 @@ namespace Project_PRN.Controllers
         }
 
         // GET: Products/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
+        public ActionResult Delete(int? id) {
+            if (id == null) {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Product product = db.Products.Find(id);
-            if (product == null)
-            {
+            if (product == null) {
                 return HttpNotFound();
             }
             return View(product);
@@ -204,18 +198,15 @@ namespace Project_PRN.Controllers
         // POST: Products/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
+        public ActionResult DeleteConfirmed(int id) {
             Product product = db.Products.Find(id);
             db.Products.Remove(product);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
+        protected override void Dispose(bool disposing) {
+            if (disposing) {
                 db.Dispose();
             }
             base.Dispose(disposing);
