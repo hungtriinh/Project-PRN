6342652b53ff841e9a9d5c9034f8bf3d4cb2a992
huySaoKim <>
2021-03-24 19:17:36 +0700

    24/3/2021 7:13PM
    Tiếp tục hoàn thiện Product

--- Project PRN/Project PRN/Controllers/ProductsController.cs
+++ Project PRN/Project PRN/Controllers/ProductsController.cs
@@ -11,23 +11,30 @@ using PagedList;
 using Project_PRN.Models;
 using System.Configuration;
 
-namespace Project_PRN.Controllers {
-    public class ProductsController : Controller {
+namespace Project_PRN.Controllers
+{
+    public class ProductsController : Controller
+    {
         private ProjectPRNEntities3 db = new ProjectPRNEntities3();
 
         // GET: Products
-        public ActionResult Product() {
+        public ActionResult Product(int? categoryID)
+        {
+            if (categoryID != null)
+            {
+                ViewData["categoryID"] = categoryID;
+            }
+            int totalCount = listProduct().Count();
+            int? totalpage = (totalCount / 9) + 1;
+            ViewData["totalpage"] = totalpage;
             return View();
+            
         }
 
-        public JsonResult ProductJson(int? index, string searchText, int? userID) {
-            if (index == null) {
-                index = 1;
-            }
-            int pageSize = 9;
-            int pageNumber = (index ?? 1);
-            db.Configuration.ProxyCreationEnabled = false;
-            var listProduct = db.Products.ToList().Select(product => new Product {
+        public List<Product> listProduct()
+        {
+            List<Product> listProduct = db.Products.ToList().Select(product => new Product
+            {
                 productID = product.productID,
                 title = product.title,
                 author = product.author,
@@ -45,21 +52,97 @@ namespace Project_PRN.Controllers {
                 Category = db.Categories.Find(product.categoriesID),
                 Evaluates = db.Evaluates.Where(e => e.productID == product.productID).ToList()
 
-            }).OrderBy(product => product.postTime).ToPagedList(pageNumber, pageSize);
+            }).OrderBy(product => product.postTime).ToList();
+
+
+
+
+            return listProduct;
+        }
+
+        public JsonResult ProductJson(int? index, string searchText, int? userID, int? categoryID)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            List<Product> listProduct;
+            if (index == null)
+            {
+                index = 1;
+            }
+
+            if (categoryID != null)
+            {
+                int pageSize = 9;
+                int pageNumber = (index ?? 1);
+
+
+                listProduct = db.Products.ToList().Select(product => new Product
+                {
+                    productID = product.productID,
+                    title = product.title,
+                    author = product.author,
+                    description = product.description,
+                    shortDescription = product.shortDescription,
+                    image = product.fullImagePath(),
+                    price = product.price,
+                    quantity = product.quantity,
+                    sold = product.sold,
+                    postTime = product.postTime,
+                    categoriesID = product.categoriesID,
+                    userID = product.userID,
+                    rate = product.calculateRate(),
+                    Account = db.Accounts.Find(product.userID),
+                    Category = db.Categories.Find(product.categoriesID),
+                    Evaluates = db.Evaluates.Where(e => e.productID == product.productID).ToList()
+
+                }).Where(p => p.categoriesID == categoryID).OrderBy(product => product.postTime).ToPagedList(pageNumber, pageSize).ToList();
+            }
+            else
+            {
+                int pageSize = 9;
+                int pageNumber = (index ?? 1);
+                listProduct = db.Products.ToList().Select(product => new Product
+                {
+                    productID = product.productID,
+                    title = product.title,
+                    author = product.author,
+                    description = product.description,
+                    shortDescription = product.shortDescription,
+                    image = product.fullImagePath(),
+                    price = product.price,
+                    quantity = product.quantity,
+                    sold = product.sold,
+                    postTime = product.postTime,
+                    categoriesID = product.categoriesID,
+                    userID = product.userID,
+                    rate = product.calculateRate(),
+                    Account = db.Accounts.Find(product.userID),
+                    Category = db.Categories.Find(product.categoriesID),
+                    Evaluates = db.Evaluates.Where(e => e.productID == product.productID).ToList()
+
+                }).OrderBy(product => product.postTime).ToPagedList(pageNumber, pageSize).ToList();
+            }
+
             return Json(listProduct, JsonRequestBehavior.AllowGet);
 
         }
 
-        public ActionResult UploadProduct() {
+        public ActionResult UploadProduct()
+        {
             db.Configuration.ProxyCreationEnabled = false;
-            if (Session["user"] == null) {
+            if (Session["user"] == null)
+            {
                 return RedirectToAction("SignIn", "Accounts");
-            } else {
+            }
+            else
+            {
                 int userID = Int32.Parse(Session["user"].ToString());
                 Account account = db.Accounts.Find(userID);
-                if (account.role == 3) {
-                    return View(); 
-                } else {
+                if (account.role == 3)
+                {
+                    return View();
+                }
+                else
+                {
                     return RedirectToAction("Index", "Home");
                 }
             }
@@ -70,13 +153,17 @@ namespace Project_PRN.Controllers {
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "productID,title,author,description,shortDescription,image,price,quantity,sold,postTime,categoriesID,userID")] Product product, HttpPostedFileBase image) {
-            if (ModelState.IsValid) {
+        public ActionResult Create([Bind(Include = "productID,title,author,description,shortDescription,image,price,quantity,sold,postTime,categoriesID,userID")] Product product, HttpPostedFileBase image)
+        {
+            if (ModelState.IsValid)
+            {
                 product.userID = Int32.Parse(Session["user"].ToString());
                 product.postTime = DateTime.Now;
                 string imgPath = ConfigurationManager.ConnectionStrings["imagePath"].ToString();
-                try {
-                    if (image != null) {
+                try
+                {
+                    if (image != null)
+                    {
                         var allowedExtensions = new[] { ".Jpg", ".png", ".jpg", "jpeg" };
                         var ext = Path.GetExtension(image.FileName);
                         if (allowedExtensions.Contains(ext)) //check what type of extension  
@@ -84,18 +171,24 @@ namespace Project_PRN.Controllers {
                             string path = Path.Combine(Server.MapPath($"~{imgPath}"), Path.GetFileName(image.FileName));
                             image.SaveAs(path);
                             product.image = image.FileName;
-                        } else { return RedirectToAction("UploadProduct", "Products"); }
-                    } else {
+                        }
+                        else { return RedirectToAction("UploadProduct", "Products"); }
+                    }
+                    else
+                    {
                         return RedirectToAction("UploadProduct", "Products");
                     }
                     ViewBag.FileStatus = "File uploaded successfully.";
-                } catch (Exception) {
+                }
+                catch (Exception)
+                {
 
                 }
 
                 db.Products.Add(product);
                 db.SaveChanges();
-                return RedirectToRoute(new {
+                return RedirectToRoute(new
+                {
                     controller = "Home",
                     action = "Index",
                     id = UrlParameter.Optional
@@ -104,14 +197,17 @@ namespace Project_PRN.Controllers {
             return RedirectToAction("UploadProduct");
         }
 
-        public ActionResult ProductDetail(int? productID) {
+        public ActionResult ProductDetail(int? productID)
+        {
             ViewData["productID"] = productID;
             return View();
         }
 
-        public JsonResult ProductDetailJson(int? productID) {
+        public JsonResult ProductDetailJson(int? productID)
+        {
             db.Configuration.ProxyCreationEnabled = false;
-            Product products = db.Products.ToList().Select(product => new Product {
+            Product products = db.Products.ToList().Select(product => new Product
+            {
                 productID = product.productID,
                 title = product.title,
                 author = product.author,
@@ -127,7 +223,8 @@ namespace Project_PRN.Controllers {
                 rate = product.calculateRate(),
                 Account = db.Accounts.Find(product.userID),
                 Category = db.Categories.Find(product.categoriesID),
-                Evaluates = db.Evaluates.ToList().Select(evaluate => new Evaluate {
+                Evaluates = db.Evaluates.ToList().Select(evaluate => new Evaluate
+                {
                     evaluateID = evaluate.evaluateID,
                     evaluateContent = evaluate.evaluateContent,
                     rate = evaluate.rate,
@@ -145,26 +242,31 @@ namespace Project_PRN.Controllers {
             return Json(products, JsonRequestBehavior.AllowGet);
         }
 
-        public JsonResult GetCategoriesJson() {
+        public JsonResult GetCategoriesJson()
+        {
             db.Configuration.ProxyCreationEnabled = false;
             List<Category> listCategories = db.Categories.ToList();
             return Json(listCategories, JsonRequestBehavior.AllowGet);
         }
 
         // GET: Products/Details/5
-        public ActionResult Details(int? id) {
-            if (id == null) {
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Product product = db.Products.Find(id);
-            if (product == null) {
+            if (product == null)
+            {
                 return HttpNotFound();
             }
             return View(product);
         }
 
         // GET: Products/Create
-        public ActionResult Create() {
+        public ActionResult Create()
+        {
             ViewBag.userID = new SelectList(db.Accounts, "userID", "email");
             ViewBag.categoriesID = new SelectList(db.Categories, "categoriesID", "categoriesName");
             return View();
@@ -173,12 +275,15 @@ namespace Project_PRN.Controllers {
 
 
         // GET: Products/Edit/5
-        public ActionResult Edit(int? id) {
-            if (id == null) {
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Product product = db.Products.Find(id);
-            if (product == null) {
+            if (product == null)
+            {
                 return HttpNotFound();
             }
             ViewBag.userID = new SelectList(db.Accounts, "userID", "email", product.userID);
@@ -191,8 +296,10 @@ namespace Project_PRN.Controllers {
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "productID,title,author,description,shortDescription,image,price,quantity,sold,postTime,categoriesID,userID")] Product product) {
-            if (ModelState.IsValid) {
+        public ActionResult Edit([Bind(Include = "productID,title,author,description,shortDescription,image,price,quantity,sold,postTime,categoriesID,userID")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
                 db.Entry(product).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -203,12 +310,15 @@ namespace Project_PRN.Controllers {
         }
 
         // GET: Products/Delete/5
-        public ActionResult Delete(int? id) {
-            if (id == null) {
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Product product = db.Products.Find(id);
-            if (product == null) {
+            if (product == null)
+            {
                 return HttpNotFound();
             }
             return View(product);
@@ -217,15 +327,18 @@ namespace Project_PRN.Controllers {
         // POST: Products/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id) {
+        public ActionResult DeleteConfirmed(int id)
+        {
             Product product = db.Products.Find(id);
             db.Products.Remove(product);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
 
-        protected override void Dispose(bool disposing) {
-            if (disposing) {
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
                 db.Dispose();
             }
             base.Dispose(disposing);
